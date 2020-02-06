650458a42036057b0276c89a6c9ac85dd6ceb39c
Tanula98 <tanula98zaa@gmail.com>
2020-02-07 01:51:39 +0300

    Scripted auto-commit on change (2020-02-07 01:51:39) by gitwatch.sh

--- Documents/bmstu/Желудков/lab/lab_6/src/main/java/ZookeeperExecutor.java
+++ Documents/bmstu/Желудков/lab/lab_6/src/main/java/ZookeeperExecutor.java
@@ -1,7 +1,12 @@
+import akka.actor.ActorRef;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
 
 public class ZookeeperExecutor implements Watcher {
+
+    private ZooKeeper zoo;
+    private ActorRef cacheActor;
 
     @Override
     public void process(WatchedEvent watchedEvent) {
