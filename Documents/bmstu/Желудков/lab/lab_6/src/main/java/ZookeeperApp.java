2f510ea4d6959195c1ecc78695c551027fead8c8
Tanula98 <tanula98zaa@gmail.com>
2020-02-07 01:02:15 +0300

    Scripted auto-commit on change (2020-02-07 01:02:15) by gitwatch.sh

--- Documents/bmstu/Желудков/lab/lab_6/src/main/java/ZookeeperApp.java
+++ Documents/bmstu/Желудков/lab/lab_6/src/main/java/ZookeeperApp.java
@@ -1,2 +1,11 @@
+import org.apache.zookeeper.KeeperException;
+
+import java.io.IOException;
+
 public class ZookeeperApp {
+
+    public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
+
+
+    }
 }
