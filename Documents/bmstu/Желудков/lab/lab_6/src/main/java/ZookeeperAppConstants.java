932def62ccdd089eab000c9c99793f9f18403482
Tanula98 <tanula98zaa@gmail.com>
2020-02-07 01:43:58 +0300

    Scripted auto-commit on change (2020-02-07 01:43:58) by gitwatch.sh

--- Documents/bmstu/Желудков/lab/lab_6/src/main/java/ZookeeperAppConstants.java
+++ Documents/bmstu/Желудков/lab/lab_6/src/main/java/ZookeeperAppConstants.java
@@ -9,9 +9,7 @@ public class ZookeeperAppConstants {
     static final String WATCHER_MESSAGE = "Servers list has been updated";
     static final String REDIRECT_MESSAGE = "Redirect to ";
     static final String FETCH_MESSAGE = "fetch ";
-
-    static final String CACHE_ACTOR_NAME = "cacheActor";
-
+    
 
     // HTTP query parameters
     static final String URL_PARAMETER_NAME = "url";
