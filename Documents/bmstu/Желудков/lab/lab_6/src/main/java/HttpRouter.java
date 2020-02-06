17c09420a4031f18b08d868a1087845e72d5db17
Tanula98 <tanula98zaa@gmail.com>
2020-02-07 01:29:16 +0300

    Scripted auto-commit on change (2020-02-07 01:29:16) by gitwatch.sh

--- Documents/bmstu/Желудков/lab/lab_6/src/main/java/HttpRouter.java
+++ Documents/bmstu/Желудков/lab/lab_6/src/main/java/HttpRouter.java
@@ -13,6 +13,7 @@ import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
 
 import java.util.concurrent.CompletionStage;
+import java.util.regex.Pattern;
 
 public class HttpRouter extends AllDirectives {
 
@@ -54,6 +55,6 @@ public class HttpRouter extends AllDirectives {
     ////(от актора хранилища конфигурации) и делает запрос к нему с аналогичными
     ////query параметрами (url, counter) но счетчиком на 1 меньше.
     private CompletionStage<HttpResponse> redirect(Http http, String url, int count) {
-
+        return Pattern.
     }
 }
