bc4ca498cd6dc2729ae6a63905c4145c00b0572d
Tanula98 <tanula98zaa@gmail.com>
2020-02-07 01:55:43 +0300

    Scripted auto-commit on change (2020-02-07 01:55:43) by gitwatch.sh

--- Documents/bmstu/Желудков/lab/lab_6/src/main/java/ZookeeperExecutor.java
+++ Documents/bmstu/Желудков/lab/lab_6/src/main/java/ZookeeperExecutor.java
@@ -16,14 +16,13 @@ public class ZookeeperExecutor implements Watcher {
     public void process(WatchedEvent watchedEvent) {
 
         try {
-
             List<String> serversNodes = zoo.getChildren(ZookeeperAppConstants.SERVERS_NODE, this);
             List<String> serversList = new ArrayList<>();
 
             for (String s : serversNodes){
-
+                byte[] server = zoo.getData(ZookeeperAppConstants.SERVERS_NODES_PATH + s, false, null);
+                serversList.add(new String(server));
             }
-
 
         } catch (KeeperException | InterruptedException e){
             e.printStackTrace();
