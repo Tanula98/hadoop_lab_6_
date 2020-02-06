e0894706e6ffa836c32745682fe904dd4325e7c1
Tanula98 <tanula98zaa@gmail.com>
2020-02-07 01:22:40 +0300

    Scripted auto-commit on change (2020-02-07 01:22:40) by gitwatch.sh

--- Documents/bmstu/Желудков/lab/lab_6/src/main/java/HttpRouter.java
+++ Documents/bmstu/Желудков/lab/lab_6/src/main/java/HttpRouter.java
@@ -25,7 +25,7 @@ public class HttpRouter extends AllDirectives {
 
     Route createRoute(Http http) {
         return route(
-                get(() -> parameter(ZookeeperAppConstants.URL_PARAMETER_NAME, (url))
+                get(() -> parameter(ZookeeperAppConstants.URL_PARAMETER_NAME, (url) -> )
 
                 )
         );
