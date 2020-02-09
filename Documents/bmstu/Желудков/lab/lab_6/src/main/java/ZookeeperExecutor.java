d12f3903032f4a6b9822f5f9d3b0adeabcb51746
Tanula98 <tanula98zaa@gmail.com>
2020-02-09 16:02:49 +0300

    Scripted auto-commit on change (2020-02-09 16:02:48) by gitwatch.sh

--- Documents/bmstu/Желудков/lab/lab_6/src/main/java/ZookeeperExecutor.java
+++ Documents/bmstu/Желудков/lab/lab_6/src/main/java/ZookeeperExecutor.java
@@ -11,15 +11,17 @@ public class ZookeeperExecutor implements Watcher {
 
     ZookeeperExecutor(int serverPort) throws IOException, KeeperException, InterruptedException {
 
+        //создаем доступ к zookeeper
         zoo = new ZooKeeper(
                 ZookeeperAppConstants.ZOOKEEPER_SERVER + ":" + ZookeeperAppConstants.ZOOKEEPER_PORT,
                 ZookeeperAppConstants.ZOOKEEPER_SESSION_TIMEOUT,
                 this
         );
 
-
+        //собираем адрес текущего сервера
         String serverUrl = "http://" + ZookeeperAppConstants.HOST + ":" + serverPort;
 
+        //регистрируемся в зоокипере
         zoo.create(
                 ZookeeperAppConstants.SERVER_NODE,
                 serverUrl.getBytes(),
@@ -32,9 +34,11 @@ public class ZookeeperExecutor implements Watcher {
     public void process(WatchedEvent watchedEvent) {
 
         try {
+            //получаем спиок серверов
             List<String> serversNodes = zoo.getChildren(ZookeeperAppConstants.SERVERS_NODE, this);
             List<String> serversList = new ArrayList<>();
 
+            //для каждого сервера получаем адрес
             for (String s : serversNodes){
                 byte[] server = zoo.getData(ZookeeperAppConstants.SERVERS_NODES_PATH + s, false, null);
                 serversList.add(new String(server));
